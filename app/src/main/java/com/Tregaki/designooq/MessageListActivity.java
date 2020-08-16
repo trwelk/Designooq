d55f3628593bdefca88c54b1d6d94dab79d37e52
trwelk <trewon.weerasooriya@ifs.com>
2020-08-16 14:38:10 +0530

    Changed few things

--- app/src/main/java/com/Tregaki/designooq/MessageListActivity.java
+++ app/src/main/java/com/Tregaki/designooq/MessageListActivity.java
@@ -4,9 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import com.google.android.material.tabs.TabItem;
 import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class MessageListActivity extends AppCompatActivity {
     private Toolbar mainTooldbar;
@@ -14,6 +21,24 @@ public class MessageListActivity extends AppCompatActivity {
     private TabItem tabFriends;
     private TabItem tabPosts;
     private TabLayout mTabLayout;
+
+    // Array of strings for ListView Title
+    String[] listviewTitle = new String[]{
+            "ListView Title 1", "ListView Title 2", "ListView Title 3", "ListView Title 4",
+            "ListView Title 5",
+    };
+
+
+    int[] listviewImage = new int[]{
+            R.drawable.account_icon_tab, R.drawable.account_icon_tab, R.drawable.account_icon_tab, R.drawable.account_icon_tab,
+            R.drawable.account_icon_tab,
+    };
+
+    String[] listviewShortDescription = new String[]{
+            "Android ListView Short Description", "Android ListView Short Description", "Android ListView Short Description", "Android ListView Short Description",
+            "Android ListView Short Description",
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +47,35 @@ public class MessageListActivity extends AppCompatActivity {
         tabFriends = (TabItem) findViewById(R.id.account_tab);
         tabPosts = (TabItem) findViewById(R.id.posts_tab);
         mTabLayout =(TabLayout)  findViewById(R.id.main_tab_layout);
+        mainTooldbar = (Toolbar) findViewById(R.id.main_page_toolbar);
 
+        setSupportActionBar(mainTooldbar);
+        getSupportActionBar().setTitle("Chat List");
+
+
+        List<HashMap<String, String>> aList = new ArrayList<HashMap<String, String>>();
+
+        for (int i = 0; i < 5; i++) {
+            HashMap<String, String> hm = new HashMap<String, String>();
+            hm.put("listview_title", listviewTitle[i]);
+            hm.put("listview_discription", listviewShortDescription[i]);
+            hm.put("listview_image", Integer.toString(listviewImage[i]));
+            aList.add(hm);
+        }
+
+        String[] from = {"listview_image", "listview_title", "listview_discription"};
+        int[] to = {R.id.listview_image, R.id.listview_item_title, R.id.listview_item_short_description};
+
+        SimpleAdapter simpleAdapter = new SimpleAdapter(getBaseContext(), aList, R.layout.listview_activity, from, to);
+        ListView androidListView = (ListView) findViewById(R.id.list_view);
+        androidListView.setAdapter(simpleAdapter);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return  true;
     }
 }
