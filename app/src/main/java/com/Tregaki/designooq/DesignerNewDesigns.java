b1d4de4db43ddacb891e7ca114dfe55ccf1c568a
admin <kk482933@gmail.com>
2020-08-17 11:55:47 +0530

    Kishore Commit

---
+++ app/src/main/java/com/Tregaki/designooq/DesignerNewDesigns.java
@@ -0,0 +1,17 @@
+package com.Tregaki.designooq;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DesignerNewDesigns  extends  AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_designer_newdesign);
+    }
+}
+
+
+
+
