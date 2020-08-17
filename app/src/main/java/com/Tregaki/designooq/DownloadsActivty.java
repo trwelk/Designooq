da9e3d735de6dab0388be5f2616abc2bde9e8327
trwelk <trewon.weerasooriya@ifs.com>
2020-08-17 11:00:04 +0530

    Strings added and downs

--- app/src/main/java/com/Tregaki/designooq/DownloadsActivty.java
+++ app/src/main/java/com/Tregaki/designooq/DownloadsActivty.java
@@ -3,6 +3,7 @@ package com.Tregaki.designooq;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import com.github.mikephil.charting.charts.PieChart;
@@ -13,19 +14,24 @@ import com.github.mikephil.charting.data.PieEntry;
 import java.util.ArrayList;
 import java.util.List;
 
-public class DesignerHomeActivity extends AppCompatActivity {
+public class DownloadsActivty extends AppCompatActivity {
+
+    public int colors[] = {
+            Color.rgb(193,37,82),            Color.rgb(255,102,0),            Color.rgb(245,199,0),
+            Color.rgb(106,150,31),            Color.rgb(179,100,53)
+    };
 
     float downloads[] = {20,32,13,43,12,32,43,23,32,12,32,12};
     String months[] = {"Jan","Feb","march","april","may","June","July","Aug","Sept","SAM","Nov","Dec"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_designer_home);
+        setContentView(R.layout.activity_downloads_activty);
+
         Toolbar mainTooldbar = (Toolbar) findViewById(R.id.main_page_toolbar);
 
         setSupportActionBar(mainTooldbar);
-        getSupportActionBar().setTitle("My Home");
-        
+        getSupportActionBar().setTitle("Monthly Downloads");
         setUpPieChart();
     }
 
@@ -35,11 +41,14 @@ public class DesignerHomeActivity extends AppCompatActivity {
             pieEntries.add(new PieEntry(downloads[a],months[a]));
         }
         PieDataSet dataSet = new PieDataSet(pieEntries,"Downloads per month");
-
+        dataSet.setColors(colors);
+        dataSet.setValueTextSize(20);
         PieData pieData = new PieData(dataSet);
 
         PieChart chart = (PieChart) findViewById(R.id.pir_chart);
         chart.setData(pieData);
+        chart.setCenterTextSize(20);
+
         chart.invalidate();
     }
 }
