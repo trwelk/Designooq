6d9d8b8a8c36a34b4595f5c0e5ccbc2be8cb4f50
admin <kk482933@gmail.com>
2020-09-26 09:54:38 +0530

    - Bug 's Fixed - Kishore [IT19162492]

--- app/src/main/java/com/Tregaki/designooq/DesignerNewDesigns.java
+++ app/src/main/java/com/Tregaki/designooq/DesignerNewDesigns.java
@@ -1,9 +1,12 @@
+//IT19162492
 package com.Tregaki.designooq;
 
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+//Functional Coding 
 public class DesignerNewDesigns  extends  AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState){
