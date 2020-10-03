0a614d8e18fce12796c555197c87f712ac8f109c
trwelk <trewon.weerasooriya@ifs.com>
2020-10-03 15:15:21 +0530

    UI fixes

--- app/src/test/java/com/Tregaki/designooq/ChatActivityTest.java
+++ app/src/test/java/com/Tregaki/designooq/ChatActivityTest.java
@@ -1,5 +1,55 @@
+package com.Tregaki.designooq;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ServerValue;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.junit.Assert.*;
 
 public class ChatActivityTest {
+
+    private String message;
+    private String user;
+    private String friend;
+    DatabaseReference rootDatabase;
+    @Before
+    public void setUp() throws Exception {
+         message = "This is a message";
+         user = "user1";
+         friend = "friend1";
+
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void friendIsEmpty() {
+        assertEquals(false,new ChatActivity().friendIsEmpty(friend));
+        assertEquals( true,new ChatActivity().friendIsEmpty(null));
+
+    }
+
+    @Test
+    public void senderIsEmpty() {
+        assertEquals(false,new ChatActivity().senderIsEmpty(user));
+        assertEquals(true,new ChatActivity().senderIsEmpty(null));
+    }
+
+    @Test
+    public void messageIsEmpty() {
+        assertEquals(false,new ChatActivity().messageIsEmpty(message));
+        assertEquals( true,new ChatActivity().messageIsEmpty(null));
+    }
+
 
 }
